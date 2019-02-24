dfdbb56145cfcefdeb34151120d25fa0356c9b3c
tasos-git <hbdevelop@gmail.com>
2019-02-25 01:04:34 +0200

    content added

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,11 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <SEO title="Μεταβολή Ζωής" keywords={[`αρωγή`, `αποτοξίνωση`, `ναρκωτικά`, `κέντρο αποτοξίνωσης`]} />
+    <h1>Μεταβολή Ζωής</h1>
+    <div style={{ marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
