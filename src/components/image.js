cb659bc89b7302b429d902499bc04210b059246f
tasos-git <hbdevelop@gmail.com>
2019-02-25 01:19:53 +0200

    Image to 1200

--- src/components/image.js
+++ src/components/image.js
@@ -19,7 +19,7 @@ const Image = () => (
       query {
         placeholderImage: file(relativePath: { eq: "bg.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 300) {
+            fluid(maxWidth: 1200) {
               ...GatsbyImageSharpFluid
             }
           }
