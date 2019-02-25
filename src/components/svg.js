3b9d67406821e1c9a1bdd1eafb326c47ed91aab8
tasos-git <hbdevelop@gmail.com>
2019-02-25 22:38:28 +0200

    content

--- src/components/svg.js
+++ src/components/svg.js
@@ -1,6 +1,7 @@
 import React from "react"
 
-const Svg = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="95.896" height="94.631" viewBox="0 0 95.896 94.631"><defs><style>.a{fill:#e20000;}.b{fill:#fff;}</style></defs><g transform="translate(-489.104 -644.369)"><rect width="20.549" height="34.249" transform="translate(584.977 722.171) rotate(157)"/><rect class="a" width="23.974" height="85.621" transform="matrix(-0.819, -0.574, 0.574, -0.819, 510.037, 736.299)"/><path class="a" d="M0,0H20.549a0,0,0,0,1,0,0V41.1a0,0,0,0,1,0,0h0A20.549,20.549,0,0,1,0,20.549V0A0,0,0,0,1,0,0Z" transform="matrix(-0.921, 0.391, -0.391, -0.921, 568.026, 682.201)"/><rect class="b" width="95.896" height="17.124" transform="translate(489.104 721.876)"/></g></svg>
-)
+const Svg = () => {
+  return <svg xmlns="http://www.w3.org/2000/svg" width="95.896" height="94.631" viewBox="0 0 95.896 94.631"><defs><style>.a{fill:#e20000;}.b{fill:#fff;}</style></defs><g transform="translate(-489.104 -644.369)"><rect width="20.549" height="34.249" transform="translate(584.977 722.171) rotate(157)"/><rect class="a" width="23.974" height="85.621" transform="matrix(-0.819, -0.574, 0.574, -0.819, 510.037, 736.299)"/><path class="a" d="M0,0H20.549a0,0,0,0,1,0,0V41.1a0,0,0,0,1,0,0h0A20.549,20.549,0,0,1,0,20.549V0A0,0,0,0,1,0,0Z" transform="matrix(-0.921, 0.391, -0.391, -0.921, 568.026, 682.201)"/><rect class="b" width="95.896" height="17.124" transform="translate(489.104 721.876)"/></g></svg>
+}
+
 export default Svg
