1b3cf812142b9f48b3f1f6b08ebe91d7db186851
tasos-git <hbdevelop@gmail.com>
2019-02-27 03:35:43 +0200

    messenger

--- src/components/messenger.js
+++ src/components/messenger.js
@@ -10,12 +10,13 @@ const msg = `<div id="fb-root"></div>
                 };
 
                 (function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = 'https://connect.facebook.net/el_GR/sdk/xfbml.customerchat.js';
-                fjs.parentNode.insertBefore(js, fjs);
-              }(document, 'script', 'facebook-jssdk'));</script>
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = 'https://connect.facebook.net/el_GR/sdk/xfbml.customerchat.js';
+                  fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+              </script>
               <div class="fb-customerchat"
                 attribution=setup_tool
                 page_id="341603016451170"
