e6d8ecff4f0bb7ec11f61ba963a3215973f97580
Tasos <tasos-neten@users.noreply.github.com>
2019-02-27 03:12:47 +0200

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,6 +3,32 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { StaticQuery, graphql } from "gatsby"
 
+let msg = `<div id="fb-root"></div>
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml            : true,
+        version          : 'v3.2'
+      });
+    };
+
+    (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/el_GR/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  
+  <div class="fb-customerchat"
+    attribution=setup_tool
+    page_id="341603016451170"
+    theme_color="#7646ff"
+    logged_in_greeting="Γειά σας! Πως μπορούμε να βοηθήσουμε;"
+    logged_out_greeting="Γειά σας! Πως μπορούμε να βοηθήσουμε;">
+  </div>`
+
 function SEO({ description, lang, meta, keywords, title }) {
   return (
     <StaticQuery
@@ -60,32 +86,10 @@ function SEO({ description, lang, meta, keywords, title }) {
                   : []
               )
               .concat(meta)}
-          >
-            <div id="fb-root"></div>
-            <script>
-              window.fbAsyncInit = function() {
-                FB.init({
-                  xfbml            : true,
-                  version          : 'v3.2'
-                });
-              };
-
-              (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/el_GR/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
-
-            <div class="fb-customerchat"
-              attribution=setup_tool
-              page_id="341603016451170"
-              theme_color="#7646ff"
-              logged_in_greeting="Γειά σας! Πως μπορούμε να βοηθήσουμε;"
-              logged_out_greeting="Γειά σας! Πως μπορούμε να βοηθήσουμε;">
-            </div>
-          </Helmet>
+          />
+          <div
+            dangerouslySetInnerHTML={{ __html: msg }}
+          />
         )
       }}
     />
