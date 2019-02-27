96994d01a9d60cb53f398a9978137d239bc618fa
tasos-git <hbdevelop@gmail.com>
2019-02-27 12:21:10 +0200

    messenger

--- src/components/seo.js
+++ src/components/seo.js
@@ -64,7 +64,7 @@ function SEO({ description, lang, meta, keywords, title }) {
                 )
                 .concat(meta)}
             />
-            <Messenger />
+            
           </>
         )
       }}
