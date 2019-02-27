67421eff25a7e798776da046a98a79204ad0987a
Tasos <tasos-neten@users.noreply.github.com>
2019-02-27 03:07:00 +0200

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -60,7 +60,34 @@ function SEO({ description, lang, meta, keywords, title }) {
                   : []
               )
               .concat(meta)}
-          />
+          >
+            <!-- Load Facebook SDK for JavaScript -->
+            <div id="fb-root"></div>
+            <script>
+              window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml            : true,
+                  version          : 'v3.2'
+                });
+              };
+
+              (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/el_GR/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+            <!-- Your customer chat code -->
+            <div class="fb-customerchat"
+              attribution=setup_tool
+              page_id="341603016451170"
+              theme_color="#7646ff"
+              logged_in_greeting="Γειά σας! Πως μπορούμε να βοηθήσουμε;"
+              logged_out_greeting="Γειά σας! Πως μπορούμε να βοηθήσουμε;">
+            </div>
+          </Helmet>
         )
       }}
     />
