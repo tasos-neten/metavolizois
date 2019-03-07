9cd7642f36498418dd4ee8ecbcb7f72bd7f161e3
tasos-git <hbdevelop@gmail.com>
2019-03-07 21:02:27 +0200

    youtube

--- src/components/youtube.js
+++ src/components/youtube.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const msg = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/rkg6V7qP5iI" frameborder="0" 
+const msg = `<iframe width="100%" height="450" src="https://www.youtube.com/embed/rkg6V7qP5iI" frameborder="0" 
 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 
 const Youtube = () => (
